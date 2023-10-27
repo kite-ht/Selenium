1870e4d3c54d3ce8def97264cb428501b7efe196
kite-ht <kietht18032003@gmail.com>
2023-10-28 00:09:13 +0700

    TC01, TC02

---
+++ selenium-webdriver-java/src/test/java/BaiTap/TC01/TC01.java
@@ -0,0 +1,48 @@
+package BaiTap.TC01;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+import driver.driverFactory;
+
+import java.io.File;
+
+public class TC01 {
+    @Test
+    public void test() {
+        // 1. Init web-driver session
+        WebDriver driver = driverFactory.getChromeDriver();
+        try {
+            // 2. Open target page - Login Form
+            driver.get("http://live.techpanda.org/index.php/");
+            // 3: Verify Title of the page
+            String expectedTitle = "Home page";
+            String actualTitle = driver.getTitle();
+            if (actualTitle.contains(expectedTitle)) {
+                System.out.println("Title matches: " + actualTitle);
+            } else {
+                System.out.println("Title doesn't match: " + actualTitle);
+            }
+            // 4: Click on MOBILE menu
+            WebElement mobileMenu = driver.findElement(By.linkText("MOBILE"));
+            mobileMenu.click();
+            TakesScreenshot beforeSC =((TakesScreenshot)driver);
+            File srcFile1= beforeSC.getScreenshotAs(OutputType.FILE);
+            FileHandler.copy(srcFile1, new File("/Users/hatuankiet/Downloads/selenium-webdriver-java/src/test/java/BaiTap/TC01/BeforeSort.png"));
+            // 5: Select SORT BY dropdown as name
+            WebElement sortDropdown = driver.findElement(By.xpath("//select[@title='Sort By']"));
+            Select sortSelect = new Select(sortDropdown);
+            sortSelect.selectByVisibleText("Name");
+            // 6: Take a screenshot after sort
+            TakesScreenshot afterSC =((TakesScreenshot)driver);
+            File srcFile2= afterSC.getScreenshotAs(OutputType.FILE);
+            FileHandler.copy(srcFile2, new File("/Users/hatuankiet/Downloads/selenium-webdriver-java/src/test/java/BaiTap/TC01/AfterSort.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 7. Quit browser session
+        driver.quit();
+        // }
+    }
+}
